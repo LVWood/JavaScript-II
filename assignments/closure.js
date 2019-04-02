aaea61fe673f15c3786fdd278aff96a15b793c7c
LVWood <leslee.wood@gmail.com>
2019-04-02 15:29:55 -0500

    closure assignment done.

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,28 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+function myHouse() {
+  const address = "Cedar St";
+  console.log(`We live on ${address}.`);
+  //debugger;
+
+  function upstairs() {
+    const privateSpace = "rooms";
+    console.log(`We live together in a house on ${address}, but we all have our own ${privateSpace}.`);
+    //debugger;
+    
+    function myOffice() {
+      const offLimits = "Mom's office";
+      console.log(`We live together on ${address} and we have our own ${privateSpace}. The kids know they're not allowed in ${offLimits}.`);
+      //debugger;
+    }
+    myOffice();
+  }
+  upstairs();
+}
+myHouse();
+
+
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
